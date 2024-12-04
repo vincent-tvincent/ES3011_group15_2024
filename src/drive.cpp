25998266429d1ccf012404c1850478bf61101a52
blue_lobster <vincent.pu@outlook.com>
2024-12-04 15:08:27 -0500

    velocity to rpm impelemented but untested

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,6 +1,32 @@
 #include<drive.h>
 
+SmartMotor left = SmartMotor(LEFT_MOTOR_ADDR);
+SmartMotor right = SmartMotor(RIGHT_MOTOR_ADDR);
+
 void drive_init(){
     Serial.begin(serial_speed);
     Wire.begin();
+}
+
+void brake(){
+    left.set_rpm(0);
+    right.set_rpm(0);
+}
+
+int* drive(float velocity_input[2]){
+    float linear_velocity = velocity_input[0];
+    float angular_velocity = velocity_input[1];
+
+    // calculate linear velocity 
+    float angular_to_linear_velocity = angular_velocity * car_radius;      
+    float left_velocity = linear_velocity - angular_to_linear_velocity;
+    float right_velocity = linear_velocity + angular_to_linear_velocity; 
+    
+    float left_rpm = left_velocity / wheel_radius * LEFT_MOTOR_DIRECTION;
+    float right_rpm = right_velocity / wheel_radius * RIGHT_MOTOR_DIRECTION;
+
+    int* real_speed = (int*) malloc(sizeof(int) * 2);
+    real_speed[0] = left_rpm;
+    real_speed[1] = right_rpm;
+    return real_speed;
 }
--- src/drive.h
+++ src/drive.h
@@ -4,6 +4,11 @@
 #include <Wire.h>
 #include<smartmotor.h>
 
+// car dimentions (mm)
+#define wheel_radius 58
+#define wheel_distance 115
+#define car_radius wheel_distance / 2
+
 //serial speed 
 #define serial_speed 115200
 
@@ -14,12 +19,9 @@ const int32_t ENCODER_TICKS_PER_SHAFT_REV= ENCODER_TICKS_PER_REV * GEAR_RATIO;
 #define DELAY_PERIOD 0 //7000
 #define RIGHT_MOTOR_ADDR 0x0A
 #define LEFT_MOTOR_ADDR 0x0B
-
-// INIT SMART MOTORS
-// SmartMotor motors[] = {0x05,0x06,0x07}; // INIT MOTORS
-// SmartMotor motors[] = {0x0A, 0x0B};
-// const int MOTOR_NUM = sizeof(motors)/sizeof(motors[0]);
+#define LEFT_MOTOR_DIRECTION 1 
+#define RIGHT_MOTOR_DIRECTION -1
 void drive_init();
-void drive(float velocity_input[2]); //TODO 
+int* drive(float velocity_input[2]); //TODO 
 void brake(); //TODO
 #endif
